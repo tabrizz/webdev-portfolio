53ab053cf0fe48a7f09ef39f3430a9ba091fca91
tabrizz <kjetlag@gmail.com>
2019-07-19 18:33:18 -0500

    inital commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import "../styles/main.scss";
+
+const index = () => {
+  return (
+    <div className="bg-black">
+      <h1 className="text-white">Hello World</h1>
+    </div>
+  );
+};
+
+export default index;
